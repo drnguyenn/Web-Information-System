c35a72fc7a7049bbc90a590af208e2b46d9866bc
NGUYEN DICH LONG <longnguyen25111998@gmail.com>
2019-12-23 15:00:32 +0700

    Complete weekly homeworks

--- sample_app/config/application.rb
+++ sample_app/config/application.rb
@@ -1,19 +1,22 @@
 require_relative 'boot'
-
+require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module SampleApp
+module RubyApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 5.1
+
+    # Include the authenticity token in remote forms.
+    config.action_view.embed_authenticity_token_in_remote_forms = true
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
   end
 end
+
--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,7 +1,33 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
+  get 'sessions/new'
+
+  # get 'users/new'
+
   root 'static_pages#home'
-  get 'help' => 'static_pages#help'
-  get 'about' => 'static_page#about'
-  get 'contact' => 'static_pacge#contact'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+  
+
+  get '/signup', to: 'users#new'
+  post '/signup',  to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+
+  resources :users
+  resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 end
